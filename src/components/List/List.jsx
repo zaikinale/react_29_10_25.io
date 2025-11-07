08861868b0b53fa29047aa9c387beb18df0398b6
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-11-07 14:59:14 +0300

    fix_design

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,36 +1,66 @@
 import { useState } from 'react';
 import style from './style.module.css';
 
+import AlterIcon from '../../assets/alter.svg'
+import CheckIcon from '../../assets/check.svg'
+import RemoveIcon from '../../assets/remove.svg'
+import SaveIcon from '../../assets/save.svg'
+import TrashIcon from '../../assets/trash.svg'
+
 const Item = ({ item, onRemove, onUpdate, onToggleComplete }) => {
     const [isEditing, setIsEditing] = useState(false);
     const [editText, setEditText] = useState(item.text);
     const [editDeadline, setEditDeadline] = useState(item.deadline || '');
     const [editTags, setEditTags] = useState(item.tags?.join(', ') || '');
 
+    const handleSave = () => {
+        const tags = editTags
+            .split(',')
+            .map(tag => tag.trim())
+            .filter(tag => tag);
+        onUpdate(item.id, {
+            text: editText.trim(),
+            deadline: editDeadline || null,
+            tags: tags
+        });
+        setIsEditing(false);
+    };
+
+    const handleEdit = () => {
+        setEditText(item.text);
+        setEditDeadline(item.deadline || '');
+        setEditTags(item.tags?.join(', ') || '');
+        setIsEditing(true);
+    };
+
     const renderData = () => {
         const deadlineStr = item.deadline 
             ? `Deadline: ${new Date(item.deadline).toLocaleDateString('ru-RU')}` 
             : '';
-        const tagsStr = item.tags?.length ? `Tags: ${item.tags.join(', ')}` : '';
-    
+        const tagsStr = item.tags?.length ? `#${item.tags.join(' #')}` : '';
+
         return (
             <>
                 {(deadlineStr || tagsStr) && (
                     <div className={style.item__meta}>
-                        {deadlineStr} <br/> {tagsStr && ` ${tagsStr}`}
+                        {deadlineStr}  {tagsStr && ` ${tagsStr}`}
                     </div>
                 )}
-                <span>{item.text}</span><br/>
+                <span>{item.text}</span>
 
-                <div className="btnControl">
+                <div className={style.btnControl}>
                     <button className={style.list__delete} onClick={() => onRemove(item.id)}>
-                        Удалить
+                        <img className={style.btnControlImg} src={TrashIcon} alt='Удалить' />
+                        
                     </button>
                     <button className={style.list__completed} onClick={() => onToggleComplete(item.id)}>
-                        {item.status === 'completed' ? 'Отменить' : 'Выполнено'}
+                        <img className={style.btnControlImg} src={item.status === 'completed' ? RemoveIcon : CheckIcon } alt={item.status === 'completed' ? 'Отменить' : 'Выполнено'} />
+                        
+                    </button>
+                    <button className={style.list__fix} onClick={handleEdit}>
+                        <img className={style.btnControlImg} src={AlterIcon} alt='Изменить' />
                     </button>
                 </div>
-
             </>
         );
     };
@@ -56,27 +86,19 @@ const Item = ({ item, onRemove, onUpdate, onToggleComplete }) => {
                 placeholder="Теги (через запятую)"
                 className={style.editor__input}
             />
+            <div className={style.btnControl}>
+                <button className={style.list__delete} onClick={() => onRemove(item.id)}>
+                    <img className={style.btnControlImg} src={RemoveIcon} alt="Удалить" />
+                </button>
+                <button className={style.list__completed} onClick={() => onToggleComplete(item.id)}>
+                    <img className={style.btnControlImg} src={item.status === 'completed' ? RemoveIcon : CheckIcon } alt={item.status === 'completed' ? 'Отменить' : 'Выполнено'} />
+                </button>
+                <button className={style.list__fix} onClick={handleSave}>
+                    <img className={style.btnControlImg} src={SaveIcon} alt="Сохранить" />
+                </button>
+            </div>
         </>
     );
-
-    const modeSwitching = () => {
-        if (isEditing) {
-            const tags = editTags
-                .split(',')
-                .map(tag => tag.trim())
-                .filter(tag => tag);
-            onUpdate(item.id, {
-                text: editText.trim(),
-                deadline: editDeadline || null,
-                tags: tags
-            });
-        } else {
-            setEditText(item.text);
-            setEditDeadline(item.deadline || '');
-            setEditTags(item.tags?.join(', ') || '');
-        }
-        setIsEditing(!isEditing);
-    };
 
     let itemClass = style.list__item;
     if (item.status === 'completed') {
@@ -88,9 +110,6 @@ const Item = ({ item, onRemove, onUpdate, onToggleComplete }) => {
     return (
         <li className={itemClass} key={item.id}>
             {isEditing ? renderEditor() : renderData()}
-            <button className={style.list__fix} onClick={modeSwitching}>
-                {isEditing ? 'Сохранить' : 'Изменить'}
-            </button>
         </li>
     );
 };
