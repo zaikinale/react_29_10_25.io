1a2786a57dcf460397995280e4b7dcc9984a0a32
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-12-08 15:14:08 +0300

    added input search in calendar

--- src/components/CalendarHeader/index.jsx
+++ src/components/CalendarHeader/index.jsx
@@ -1,13 +1,19 @@
 import style from './style.module.css';
 import SearchIcon from '../../assets/media/icons/search.svg'
 import {useType} from "../../context/useContextCalendar.jsx";
+import {useState} from "react";
 
 export default function CalendarHeader () {
     const { typeQuery, setTypeQuery }= useType();
+    const [ isSearch, setIsSearch ] = useState(false)
 
     const handleChange = (e) => {
         setTypeQuery(e.target.value);
     };
+
+    function handleSearch () {
+        setIsSearch(state => !state)
+    }
 
     return (
         <section className={style.headerContainer}>
@@ -20,9 +26,10 @@ export default function CalendarHeader () {
                 <h2 className={style.thisMonth}>Декабрь</h2>
             </div>
             <div className={style.searchAndSelect}>
-                <button className={style.searchBtn}>
+                <button className={style.searchBtn} onClick={handleSearch}>
                     <img src={SearchIcon} alt="Поиск"/>
                 </button>
+                {isSearch && <input className={style.searchInput} type={"search"} placeholder={'Найти задачу'} /> }
                 <select name="select" className={style.selectDate} onChange={handleChange}>
                     <option value="day">День</option>
                     <option value="week" selected >Неделя</option>
